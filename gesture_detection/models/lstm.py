d46c845612581731d624bc449f034fa1c38a3638
AreopagX <49414432+AreopagX@users.noreply.github.com>
2024-01-10 09:16:59 +0100

    fix loss weight issue, move to MBNv3-S

--- gesture_detection/models/lstm.py
+++ gesture_detection/models/lstm.py
@@ -9,7 +9,7 @@ from lightning.pytorch.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
 from torchmetrics import F1Score, Accuracy, ConfusionMatrix
 
 
-class Baseline(L.LightningModule):
+class LSTM(L.LightningModule):
 
     def __init__(
             self,
@@ -23,7 +23,8 @@ class Baseline(L.LightningModule):
         self.backbone_lr = backbone_lr
         self.weight_decay = weight_decay
         self.loss_weight = nn.Parameter(torch.tensor(loss_weight)) if loss_weight is not None else None
-        self.num_features = 576
+        # self.num_features = 960  # MBv3-L
+        self.num_features = 576  # MBv3-S
         self.num_classes = num_classes
 
         self.backbone = torchvision.models.mobilenet_v3_small(torchvision.models.MobileNet_V3_Small_Weights.DEFAULT)
@@ -31,7 +32,8 @@ class Baseline(L.LightningModule):
         #    *[module for idx, module in enumerate(self.backbone.features.children()) if idx < 14]
         #)
         self.backbone.classifier = nn.Identity()
-        self.linear1 = nn.Linear(self.num_features, self.num_classes)
+
+        self.sequence_model = nn.LSTM(self.num_features, self.num_classes, 1, batch_first=True, dropout=0.05)
 
         self.metric_config = {
             "acc": (Accuracy, {"task": "multiclass", "num_classes": num_classes}),
@@ -49,15 +51,15 @@ class Baseline(L.LightningModule):
         batch_size, time_steps, channels, height, width = x.shape
         x = x.flatten(0, 1)
         x = self.backbone(x)
-        x = self.linear1(x)
-        return x
+        x = x.unflatten(0, (batch_size, time_steps))
+        out, (hn, cn) = self.sequence_model(x)
+        return out
 
     def training_step(self, batch, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
         inputs, targets = batch
-        batch_size, sample_length = inputs.shape[:2]
-        outputs = self(inputs)  # [batch_size * sample_length, num_classes
-
+        outputs = self(inputs).flatten(0, 1)  # [batch_size * sample_length, num_classes
         targets = targets.flatten(0, 1)
+
         loss = torch.nn.functional.cross_entropy(outputs, targets, weight=self.loss_weight)
 
         self.log("train_loss", loss, on_step=True, on_epoch=True)
@@ -70,10 +72,9 @@ class Baseline(L.LightningModule):
 
     def validation_step(self, batch, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
         inputs, targets = batch
-        batch_size, sample_length = inputs.shape[:2]
-        outputs = self(inputs)  # [batch_size * sample_length, num_classes
-
+        outputs = self(inputs).flatten(0, 1)  # [batch_size * sample_length, num_classes
         targets = targets.flatten(0, 1)
+
         loss = torch.nn.functional.cross_entropy(outputs, targets, weight=self.loss_weight)
 
         self.log("valid_loss", loss, on_step=True, on_epoch=True)
@@ -86,10 +87,9 @@ class Baseline(L.LightningModule):
 
     def test_step(self, batch, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
         inputs, targets = batch
-        batch_size, sample_length = inputs.shape[:2]
-        outputs = self(inputs)  # [batch_size * sample_length, num_classes
-
+        outputs = self(inputs).flatten(0, 1)  # [batch_size * sample_length, num_classes
         targets = targets.flatten(0, 1)
+
         loss = torch.nn.functional.cross_entropy(outputs, targets, weight=self.loss_weight)
 
         stage = "test"
@@ -102,13 +102,13 @@ class Baseline(L.LightningModule):
     def configure_optimizers(self) -> OptimizerLRScheduler:
         opt = torch.optim.Adam([
                 {"params": self.backbone.parameters(), "lr": self.backbone_lr},
-                {"params": self.linear1.parameters()}
+                {"params": self.sequence_model.parameters()},
             ], lr=self.lr, weight_decay=self.weight_decay)
         return opt
 
 def main():
-    model = Baseline()
-    out = model(torch.rand((1, 5, 3, 160, 160)))
+    model = LSTM(14)
+    out = model(torch.rand((2, 5, 3, 160, 160)))
     preds = F.softmax(out, dim=-1).argmax(dim=-1).squeeze(0).detach().numpy()
     print(f"predicted classes: {preds}")
 
